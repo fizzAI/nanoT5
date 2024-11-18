7e0dd47f715155611edbc9518303b91efd975069
fizzAI <fizzarolli@riseup.net>
2024-11-17 21:37:48 -0500

    i think it genuinely started training on the cpu

--- nanoT5/main.py
+++ nanoT5/main.py
@@ -21,6 +21,8 @@ from .utils import (
 
 @hydra.main(config_path="configs", config_name="default", version_base="1.1")
 def main(args):
+    torch.set_default_device("cuda")
+
     logger = setup_basics(args)
     tokenizer = get_tokenizer(args)
 
